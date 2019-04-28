8dfcd4abfb040064bc3f039ead541e95bc7cbf9a
iceihehe <iceihehe@gmail.com>
2019-04-28 15:55:18 +0800

    Update __init__.py
    The key of cas_username in flask.session should be read from app.config.

--- flask_cas/__init__.py
+++ flask_cas/__init__.py
@@ -95,7 +95,8 @@ def logout():
 def login_required(function):
     @wraps(function)
     def wrap(*args, **kwargs):
-        if 'CAS_USERNAME' not in flask.session:
+        cas_username_session_key = current_app.config['CAS_USERNAME_SESSION_KEY']
+        if cas_username_session_key not in flask.session:
             flask.session['CAS_AFTER_LOGIN_SESSION_URL'] = (
                 flask.request.script_root +
                 flask.request.full_path
